f89a985fcb9de95eec0c8c35e64d50abe0bfb4a6
08zhang <395871987@qq.com>
2021-12-14 15:03:10 +0800

    挂钩价差期权收益凭证测算工具

--- 衍生品应用/嵌入场内期权收益凭证定价分析/嵌入价差期权收益凭证定价分析.py
+++ 衍生品应用/嵌入场内期权收益凭证定价分析/嵌入价差期权收益凭证定价分析.py
@@ -6,17 +6,26 @@ w.start()
 # 函数区
 # 从Wind获取期权组合的历史价格，DATE日收盘后数据
 def get_option_portfolio_value_from_Wind(option_portfolio, date):
-    option_portfoio_b = w.wss(option_portfolio['B'], "close,exe_ratio,exe_price", "tradeDate="+date+";priceAdj=U;cycle=D").Data
-    option_portfoio_s = w.wss(option_portfolio['S'], "close,exe_ratio,exe_price", "tradeDate="+date+";priceAdj=U;cycle=D").Data
+    option_portfoio_b = w.wss(option_portfolio['B'], "close,exe_ratio,exe_price,exe_mode", "tradeDate="+date+";priceAdj=U;cycle=D").Data
+    option_portfoio_s = w.wss(option_portfolio['S'], "close,exe_ratio,exe_price,exe_mode", "tradeDate="+date+";priceAdj=U;cycle=D").Data
     # print(option_portfoio_b, option_portfoio_s)
     value = option_portfoio_b[0][0] * option_portfoio_b[1][0] - option_portfoio_s[0][0] * option_portfoio_s[1][0]
     number = option_portfoio_b[1][0]
-    lower_price = option_portfoio_b[2][0]  # 价差组合下限
-    higher_price = option_portfoio_s[2][0]  # 价差组合上限
-    return value, number, lower_price, higher_price
+    if option_portfoio_b[3][0] == '认购':
+        spread_type = 'BULL'
+        lower_price = option_portfoio_b[2][0]  # 价差组合下限
+        higher_price = option_portfoio_s[2][0]  # 价差组合上限
+    elif option_portfoio_b[3][0] == '认沽':
+        spread_type = 'BEAR'
+        value = option_portfoio_b[0][0] * option_portfoio_b[1][0] - option_portfoio_s[0][0] * option_portfoio_s[1][0]
+        lower_price = option_portfoio_s[2][0]  # 价差组合下限
+        higher_price = option_portfoio_b[2][0]  # 价差组合上限
+    else:
+        raise Exception("非法的期权类型")
+    return value, number, lower_price, higher_price, spread_type
 
 
-def bull_spread_income_certificate(start_date, end_date, underlying_index, underlying_asset, basic_return_rate, bull_spreads, principle, fixed_return_rate):
+def spread_option_income_certificate(start_date, end_date, underlying_index, underlying_asset, basic_return_rate, spread_options, principle, fixed_return_rate):
     '''
     # 产品基本特征
     :param start_date:
@@ -40,7 +49,7 @@ def bull_spread_income_certificate(start_date, end_date, underlying_index, under
 
     # 产品理论到期日收益情况
     # 计算价差期权组合单位价值和单位数量
-    option_value, option_number, asset_lower, asset_higher = get_option_portfolio_value_from_Wind(bull_spreads, start_date)
+    option_value, option_number, asset_lower, asset_higher, spread_type = get_option_portfolio_value_from_Wind(spread_options, start_date)
     option_portfolio_buy = option_available / option_value  # 购买价差组合的数量
     print('单位期权组合价值%.2f元' % option_value, '单位期权组合资产数量%.2f份' % option_number, '购买期权组合数量%.4f张' % option_portfolio_buy)
     # 标的指数和期权的相关价格
@@ -48,9 +57,16 @@ def bull_spread_income_certificate(start_date, end_date, underlying_index, under
     underlying_asset_start = w.wss(underlying_asset, "close", "tradeDate="+start_date+";priceAdj=U;cycle=D").Data[0][0]
     index_lower = underlying_index_start * asset_lower / underlying_asset_start  # 指数浮动区间下限
     index_higher = underlying_index_start * asset_higher / underlying_asset_start  # 指数浮动区间上限
-    print('\n挂钩指数：%s，%s日收盘点位：%.2f点（100%%）' % (underlying_index, start_date, underlying_index_start),
-          '\n浮动下限：%.2f点（%.2f%%）' % (index_lower, index_lower/underlying_index_start*100),
-          '\n浮动上限：%.2f点（%.2f%%）' % (index_higher, index_higher/underlying_index_start*100))
+    if spread_type == 'BULL':
+        print('\n挂钩指数：%s，%s日收盘点位：%.2f点（100%%）' % (underlying_index, start_date, underlying_index_start),
+              '\n浮动下限对应指数：%.2f点（%.2f%%）' % (index_lower, index_lower/underlying_index_start*100),
+              '\n浮动上限对应指数：%.2f点（%.2f%%）' % (index_higher, index_higher/underlying_index_start*100))
+    elif spread_type == 'BEAR':
+        print('\n挂钩指数：%s，%s日收盘点位：%.2f点（100%%）' % (underlying_index, start_date, underlying_index_start),
+              '\n浮动下限对应指数：%.2f点（%.2f%%）' % (index_higher, index_higher/underlying_index_start*100),
+              '\n浮动上限对应指数：%.2f点（%.2f%%）' % (index_lower, index_lower / underlying_index_start * 100))
+    else:
+        raise Exception("非法的期权类型")
     # 计算最佳情形期权投资的总盈亏
     bull_spread_max = (asset_higher - asset_lower) * option_number * option_portfolio_buy  # 期权投资最大收益率
     return_max = (bull_spread_max / principle) * 365 / N  # 年化期权投资最大收益率
@@ -63,7 +79,7 @@ def bull_spread_income_certificate(start_date, end_date, underlying_index, under
     # underlying_index_end = 6000
     return_rate = min(max((underlying_index_end - index_lower) / (index_higher - index_lower), 0), 1) * return_max  # 年化到期超额收益率
     return_value = return_rate * principle * N /365  # 实际到期应付出的超额资金
-    option_value, _, _, _ = get_option_portfolio_value_from_Wind(bull_spreads, end_date)
+    option_value, _, _, _, _ = get_option_portfolio_value_from_Wind(spread_options, end_date)
     option_return = option_portfolio_buy * option_value  # 期权投资到期盈亏
     hedge_value = option_return - return_value  # 对冲盈亏
     print('\n到期日%s挂钩指数为：%.2f点，产品到期年化收益率为：%.4f%%' % (end_date, underlying_index_end, return_rate*100+basic_return_rate*100))
@@ -78,9 +94,10 @@ if __name__ == "__main__":
     UNDERLYING_ASSET = '510300.SH'  # 挂钩对应投资资产
     BASIC_RATE_RATE = 0.015  # 产品基础收益率（最低收益率；年化，下同）
     BULL_SPREADS = {'B': '10003600.SH', 'S': '10003604.SH'}  # 牛市价差期权构成
+    BEAR_SPREADS = {'B': '10003613.SH', 'S': '10003609.sh'}
     PRINCIPLE = 10000000
     # 市场参数特征
     FIXED_RETURN_RATE = 0.032  # 相同期限固定收益产品收益率
 
-    bull_spread_income_certificate(START_DATE, END_DATE, UNDERLYING_INDEX, UNDERLYING_ASSET, BASIC_RATE_RATE,
-                                   BULL_SPREADS, PRINCIPLE, FIXED_RETURN_RATE)
+    spread_option_income_certificate(START_DATE, END_DATE, UNDERLYING_INDEX, UNDERLYING_ASSET, BASIC_RATE_RATE,
+                                     BEAR_SPREADS, PRINCIPLE, FIXED_RETURN_RATE)
